36cc6aeeac9910380be9d62f4ae5c765a2944b6b
Mo <moyancheng@gmail.com>
2022-01-10 11:25:03 +0800

    恢复JD签到插件

--- diy-part1-lede.sh
+++ diy-part1-lede.sh
@@ -16,3 +16,4 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git Boos https://github.com/Boos4721/OpenWrt-Packages.git' feeds.conf.default
